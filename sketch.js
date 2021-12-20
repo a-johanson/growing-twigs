e4fce192c8bf59ae359dfa0db59c9d66f0fc1776
arne-johanson <arne.johanson@xing.com>
2021-12-20 17:39:31 +0100

    animate growing lots of twigs from the center

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 
 class GrowthSite {
-    constructor(x, y, direction, growth_speed, ttl, generation) {
+    constructor(x, y, direction, growth_speed, ttl, generation, color) {
         this.x = x;
         this.y = y;
         this.direction = direction;
@@ -8,11 +8,14 @@ class GrowthSite {
         this.growth_speed = growth_speed;
         this.ttl = ttl;
         this.generation = generation;
+        this.color = color;
     }
 }
 
-function grow_twig(x, y, direction, growth_speed, ttl) {
-    let growth_sites = [new GrowthSite(x, y, direction, growth_speed, ttl, 0)];
+// const noise_scale = 0.1;
+
+function grow_twig(x, y, direction, growth_speed, ttl, color) {
+    let growth_sites = [new GrowthSite(x, y, direction, growth_speed, ttl, 0, color)];
 
     while (growth_sites.length > 0) {
         growth_sites = growth_step(growth_sites);
@@ -20,13 +23,13 @@ function grow_twig(x, y, direction, growth_speed, ttl) {
 }
 
 function growth_step(growth_sites) {
-    const noise_scale = 0.1;
-    const max_noise_value = 0.9; // for unbiased results using 4 octaves of noise with a decay of 0.5, use = 15.0 / 16.0;
+    // const max_noise_value = 0.9; // for unbiased results using 4 octaves of noise with a decay of 0.5, use = 15.0 / 16.0;
     const max_angle_incr = Math.PI / 16.0;
     const branching_angle = Math.PI / 6.0;
 
     growth_sites.forEach(gs => {
-        gs.direction += 2.0 * max_angle_incr * (noise(noise_scale * gs.x, noise_scale * gs.y) - 0.5 * max_noise_value);
+        // gs.direction += 2.0 * max_angle_incr * (noise(noise_scale * gs.x, noise_scale * gs.y) - 0.5 * max_noise_value);
+        gs.direction += 2.0 * max_angle_incr * (Math.random() - 0.5);
         if (Math.abs(gs.direction - gs.initial_direction) > branching_angle) {
             growth_sites.push(branch_growth_site(gs));
             gs.initial_direction = gs.direction;
@@ -48,24 +51,59 @@ function branch_growth_site(gs) {
         gs.x,
         gs.y,
         2.0 * gs.initial_direction - gs.direction,
-        gs.growth_speed - 0.25 * noise(gs.x, gs.y, 5.0),
+        gs.growth_speed,// - 0.25 * noise(noise_scale * gs.x, noise_scale * gs.y, 5.0),
         gs.ttl * 0.5,
-        gs.generation + 1
+        gs.generation + 1,
+        gs.color
     );
 }
 
 function draw_twig_segment(gs, new_x, new_y) {
     // strokeCap(ROUND);
-    stroke(0);
+    stroke(gs.color);
     strokeWeight(2.0 * Math.pow(0.75, gs.generation) + gs.ttl / 50.0);
     line(gs.x, gs.y, new_x, new_y);
 }
 
+function rand(min, max) {
+    return (max - min) * Math.random() + min;
+}
+
+let global_growth_sites = [];
+
+function add_global_growth_site() {
+    global_growth_sites.push(new GrowthSite(
+        canvas_width / 2,
+        canvas_height / 2,
+        rand(0.0, 2.0 * Math.PI),
+        rand(2, 3),
+        rand(150, 300),
+        0,
+        rand(45, 245)
+    ));
+}
+
+const canvas_width  = 1000;
+const canvas_height = 1000;
+let frame_count = 0;
+
 function setup() {
     // noiseSeed(589022682);
-    noiseDetail(4, 0.5);
-    createCanvas(800, 600);
-    background(240);
+    // noiseDetail(4, 0.5);
+    createCanvas(canvas_width, canvas_height);
+    background(0);
 
-    grow_twig(400, 100, 0.5 * Math.PI, 3, 150);
+    add_global_growth_site();
+}
+
+function draw() {
+    if (global_growth_sites.length > 0) {
+        global_growth_sites = growth_step(global_growth_sites);
+    }
+
+    frame_count += 1;
+    if (frame_count >= 60) {
+        add_global_growth_site();
+        frame_count = 0;
+    }
 }
